35908591d2b4699c6dc2a1c79284e5a32008d427
yangjian <535328110@qq.com>
2016-01-27 18:15:11 +0800

    添加测试数据

--- LabelSpacing/ViewController.m
+++ LabelSpacing/ViewController.m
@@ -15,7 +15,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSString *str = @"测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试";
+    NSString *str = @"测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试测试";
     SpaceLabel *spaceLabel = [SpaceLabel lableFrame:CGRectMake(10, 100, self.view.frame.size.width-20, 10) Content:str FontName:@"Arial" FontSize:13 title_Color:[UIColor grayColor] BgColor:[UIColor clearColor] LinesSpac:0 CharacterSpacing:1.5 ParagraphSpacing:0 TextAlignment:0];
     spaceLabel.layer.borderWidth = 1;//边框宽度
     spaceLabel.layer.borderColor = [UIColor redColor].CGColor;//边框颜色
